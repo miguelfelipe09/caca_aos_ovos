eca60e28b82c02144d293d4714440f9788eb3429
miguelfelipe09 <miguelf1618@gmail.com>
2026-03-22 21:58:41 -0300

    configurando ngrok

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,13 @@ import axios from "axios";
 import { useAuthStore } from "../store/authStore";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000",
+  headers: {
+    Accept: "application/json",
+    "Cache-Control": "no-store",
+    Pragma: "no-cache",
+    "ngrok-skip-browser-warning": "true",
+  },
 });
 
 api.interceptors.request.use((config) => {
@@ -11,8 +17,5 @@ api.interceptors.request.use((config) => {
     config.headers = config.headers ?? {};
     config.headers.Authorization = `Bearer ${token}`;
   }
-  // Evita página de aviso do ngrok retornando HTML
-  config.headers = config.headers ?? {};
-  config.headers["ngrok-skip-browser-warning"] = "true";
   return config;
 });
